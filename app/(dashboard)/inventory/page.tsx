640b99edecfefbf1206d8d8aead0075a39d8fb69
Yuvraj <106510471+yuvraj-sveltetech@users.noreply.github.com>
2024-09-05 17:16:40 +0530

    changes

--- app/(dashboard)/inventory/page.tsx
+++ app/(dashboard)/inventory/page.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Filter from "./components/Filter";
 import InventoryTable from "./components/InventoryTable";
+import { GET_INVENTORY } from "@/app/constants/apiEndpoints";
 
 const Page = async () => {
+  const tableHeaders = [
+    "Name",
+    "ID Number",
+    "Location",
+    "Total Allotted Assets",
+    "Image",
+  ];
+
   return (
     <>
       <Filter title="Inventory" />
       <hr className="my-5 text-[#B3B3B3]" />
-      <InventoryTable />
+      <InventoryTable endPoint={GET_INVENTORY} tableHeaders={tableHeaders} />
     </>
   );
 };
